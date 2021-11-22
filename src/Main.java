05fc0a2967fb21a7c04669f6a8d647f88310bfda
Sayfullah Eid <sayfullaheid@gmail.com>
2021-11-21 20:59:01 -0500

    Added documentation for `main`

--- src/Main.java
+++ src/Main.java
@@ -12,20 +12,37 @@ import static java.lang.System.out;
  * Main class
  *
  * @author Team MST
- * @version 0.1
+ * @version 0.5
  * @since 27/10/2021
  */
 
 public class Main {
-    private static Graph main = null;
+    private static Graph mainframe = null;
     private static Attack[] attacksA;
     private static Scanner in = new Scanner(System.in);
 
+    /**
+     * 
+     * @param args
+     */
     public static void main(String[] args) {
         printHeader();
-        mainMenu(in);
+        boolean run = true;
+        while (run) {
+            switch (mainMenu()) {
+            case "G" -> gMenu();
+            case "A" -> aMenu();
+            case "S" -> sMenu();
+            case "Q" -> run = false;
+            default -> out.println("Invalid selection.");
+            }
+        }
+        out.println("Goodbye!");
     }
 
+    /**
+     * Prints the header of the application to the console
+     */
     private static void printHeader() {
         out.println("""
                   ,-^-.
@@ -34,7 +51,7 @@ public class Main {
                     H
                     H        PlagueSim
                     H           v0.5
-                 .-;":-.
+                 .-;':-.
                 ,' | `; \\
                   """);
         out.println("=========================");
@@ -46,36 +63,123 @@ public class Main {
         out.println("     Sayfullah Eid");
         out.println("      Tolu Adebayo");
         out.println("=========================");
-
     }
 
-    private static String mainMenu(Scanner in) {
+    /**
+     * Prints out the main menu.
+     * 
+     * @return the user's input(choice)
+     */
+    private static String mainMenu() {
         out.println("------- MAIN MENU -------");
         out.println("Choose from these choices");
         out.println("-------------------------\n");
-        out.println("G - Setup new graph");
-        if (main != null) {
-            out.println("A - Parse in new attack file");
-            out.println("S - Enter search menu");
+        out.println("G - Graph menu");
+        if (mainframe != null) {
+            out.println("A - Parse in attack file.");
+            out.println("S - Enter search menu.");
         }
         out.println("Q - Quit program");
 
         return in.next().toUpperCase(Locale.ROOT);
     }
 
-    private static void gMenu(Scanner in) {
+    /**
+     * The graph menu, used to:
+     * <ul>
+     * <li>Parse new graph file</li>
+     * <li>Print current graph file(if not null)</li>
+     * </ul>
+     */
+    private static void gMenu() {
         boolean gRun = true;
         String input;
         while (gRun) {
-            out.println("--------GRAPH MENU--------");
+            out.println("------ GRAPH MENU -------");
             out.println("<filePath> - File path to graph file(this will clear an exisiting graph).");
-            out.println("Q - Return to main menu");
+            if (mainframe != null) {
+                out.println("P - Print current graph.");
+            }
+            out.println("Q - Return to main menu.");
 
             input = in.next();
-            if ("Q".equals(input)) {
-                gRun = false;
-            } else {
-                main = new Graph(input);
+            switch (input) {
+            case "Q" -> gRun = false;
+            case "q" -> gRun = false;
+            case "P" -> {
+                if (mainframe != null)
+                    out.println(mainframe);
+            }
+            case "p" -> {
+                if (mainframe != null)
+                    out.println(mainframe);
+            }
+            default -> mainframe = new Graph(input);
+            }
+        }
+    }
+
+    /**
+     * The attack menu:
+     * <ul>
+     * <li>Parse in new attack file</li>
+     * </ul>
+     */
+    private static void aMenu() {
+        boolean aRun = true;
+        String input;
+        while (aRun) {
+            out.println("------ ATACK MENU -------");
+            out.println("<filePath> - File path to attack file.");
+            out.println("Q - Return to main menu.");
+
+            input = in.next();
+            switch (input) {
+            case "q" -> aRun = false;
+            case "Q" -> aRun = false;
+            default -> {
+                parseAttacks(input);
+                out.println("Read in " + attacksA.length + " attacks.");
+                sortAttacks(attacksA, 0, attacksA.length - 1);
+                for (Attack attack : attacksA) {
+                    mainframe.attack(attack);
+                }
+            }
+            }
+        }
+    }
+
+    /**
+     * The search menu:
+     * <ul>
+     * <li>Lists all nodes that have been attacked.</li>
+     * <li>Lists all nodes that have a firewall.</li>
+     * <li>Lists all nodes that have a firewall and have been attacked.</li>
+     * <li>Lists all nodes that have an outbreak.</li>
+     * <li>Lists all nodes currently offline/down</li>
+     * </ul>
+     */
+    private static void sMenu() {
+        boolean sRun = true;
+        String input;
+        while (sRun) {
+            out.println("------ SEARCH MENU ------");
+            out.println("I - Lists all nodes that have been attacked.");
+            out.println("F - Lists all nodes that have a firewall.");
+            out.println("IF - Lists all nodes that have a firewall and have been attacked.");
+            out.println("O - Lists all nodes that have an outbreak.");
+            out.println("D - Lists all nodes currently offline/down");
+            out.println("Q - Return to main menu.");
+
+            input = in.next().toUpperCase(Locale.ROOT);
+            switch (input) {
+            case "I" -> out.println(mainframe.isInfected());
+            case "F" -> out.println(mainframe.hasFirewall());
+            case "IF" -> out.println(mainframe.hasAttackedFirewall());
+            case "O" -> out.println(mainframe.outbreaks());
+            case "D" -> out.println(mainframe.inactive());
+            case "Q" -> sRun = false;
+            default -> out.println("Invalid selection.");
             }
         }
     }
@@ -103,6 +207,13 @@ public class Main {
         attacks.toArray(attacksA);
     }
 
+    /**
+     * Sorts an {@code Attack[]} chronologically
+     * 
+     * @param arr   {@code Attack[]} to be sorted
+     * @param begin {@code int} starting index to be sorted
+     * @param end   {@code int} ending index to be sorted
+     */
     private static void sortAttacks(Attack[] arr, int begin, int end) {
         if (begin < end) {
             int partitionIndex = partition(arr, begin, end);
@@ -112,6 +223,14 @@ public class Main {
         }
     }
 
+    /**
+     * Supporting function for {@code sortAttacks()}
+     * 
+     * @param arr   {@code Attack[]}
+     * @param begin {@code int}
+     * @param end   {@code int}
+     * @return {@code int}
+     */
     private static int partition(Attack[] arr, int begin, int end) {
         long pivot = arr[end].toEpoch();
         int i = (begin - 1);
@@ -133,12 +252,20 @@ public class Main {
         return i + 1;
     }
 
+    /**
+     * Supporting function for testing that prints out {@code attacksA}
+     */
     private static void printAttacks() {
         for (Attack attack : attacksA) {
             System.out.println(attack);
         }
     }
 
+    /**
+     * Supporting funtion for testing tha shuffles an array of {@code Attack[]}
+     * 
+     * @param ar {@code Attack[]}
+     */
     static void shuffleArray(Attack[] ar) {
         // If running on Java 6 or older, use `new Random()` on RHS here
         Random rnd = ThreadLocalRandom.current();
